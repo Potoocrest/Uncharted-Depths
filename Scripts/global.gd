88f923701d96959797f71a3ff4beeb02782aac83
Forged Interactive <jane.staed@gmail.com>
2024-07-03 21:57:09 -0400

    Initial Commit
    - Created Godot Project
    - Set Up File System
    - Added Global Script
    - Created Empty Debug Scene

---
+++ Scripts/global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+# Global values that are not reset during scene transtions 
+# Script is loaded immediately upon game load
+
+func _ready():
+	pass # Runs once when the game first loads up
